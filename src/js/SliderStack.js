a81a29a9ade0b9a5ff36836eebb562ed6bfe0622
Int Algambra <bass.valle@gmail.com>
2020-04-05 19:20:59 +0300

    initial commit

---
+++ src/js/SliderStack.js
@@ -0,0 +1,49 @@
+class SliderStack {
+  constructor(totalSlides, perView) {
+    this.totalSlides = totalSlides;
+    this.perView = perView;
+    if (this.totalSlides <= this.perView) {
+      throw new Error('not enough slides');
+    }
+    this.current = [this.totalSlides - 1];
+    for (let i = 0; i <= this.perView; i++) {
+      this.current.push(i);
+    }
+  }
+
+  getNext() {
+    const next = this.current.slice(1);
+    if (next[next.length - 1] === this.totalSlides - 1) {
+      next.push(0);
+    } else {
+      next.push(next[next.length - 1] + 1);
+    }
+    return next;
+  }
+
+  next() {
+    this.current = this.getNext();
+    console.log(this.current);
+  }
+
+  getPrev() {
+    const prev = this.current.slice(0, this.current.length - 1);
+    if (prev[0] === 0) {
+      prev.unshift(this.totalSlides - 1);
+    } else {
+      prev.unshift(prev[0] - 1);
+    }
+    return prev;
+  }
+
+  prev() {
+    this.current = this.getPrev();
+    console.log(this.current);
+  }
+
+  getCurrent() {
+    return this.current;
+  }
+}
+
+export default SliderStack;
